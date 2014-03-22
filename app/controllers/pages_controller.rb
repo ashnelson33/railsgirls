a8853db3e31d5058883d4af5607ec92956e18345
Ashley Nelson <Ashnelson33@gmail.com>
2014-03-22 16:19:32 -0600

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def info
+  	@title = "Pages"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = '6a3974419b709eb4fcb7b75c4785616693159948f9ef6748cebe0b35b587575cc5bab726004bb66357d46f16938f59be077242b87150feb666d0289469731331'
